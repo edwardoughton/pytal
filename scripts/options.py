1f32ae7ed48017cb62ad3fa519936e428bc34d92
ej550 <edward.oughton@gmail.com>
2020-03-05 19:12:34 +0000

    Add frequencies to country_parameters

--- scripts/options.py
+++ scripts/options.py
@@ -16,10 +16,6 @@ OPTIONS = {
         {
             'scenario': 'S1_30',
             'strategy': '4G_epc_microwave_baseline',
-            'frequencies': [
-                {'frequency': 800, 'bandwidth': 10},
-                {'frequency': 2600, 'bandwidth': 10},
-            ],
         },
     #     {
     #         'scenario': 'S2_50',
@@ -189,8 +185,53 @@ OPTIONS = {
 
 COUNTRY_PARAMETERS = {
     'MWI': {
-            'penetration': 0.3,
-            'smartphone_pen': 0.15,
-            'networks': 2,
+        # Unique mobile subscribers = 30% (GSMA, 2019)
+        # https://www.gsma.com/mobilefordevelopment/wp-content/uploads/2019/02/Digital-Identity-Country-Report.pdf
+        'penetration': 0.3,
+        # also GSMA, 2019 (same report as above)
+        # smartphone pen was 10% in 2017, so assume 15% in 2020
+        'smartphone_pen': 0.15,
+        # Access Comm, Airtel, TNM
+        # https://en.wikipedia.org/wiki/List_of_LTE_networks_in_Africa
+        'networks': 3,
+        # https://en.wikipedia.org/wiki/List_of_LTE_networks_in_Africa
+        'frequencies': {
+            '1_networks': [
+                    {
+                        'name': 'Access Comm',
+                        'frequency': 800, #Actually 850 MHz but treating as 800 MHz
+                        'bandwidth': 10 #Actually 5, treating as 10 for now
+                    },
+                    {
+                        'name': 'Airtel',
+                        'frequency': 1800,
+                        'bandwidth': 3
+                    },
+                    {
+                        'name': 'TNM',
+                        'frequency': 2500,
+                        'bandwidth': 41
+                    },
+                ],
+                '2_networks': [
+                    {
+                        'name': 'Access Comm',
+                        'frequency': 800,
+                        'bandwidth': 10 #Actually 5, treating as 10 for now
+                    },
+                    {
+                        'name': 'Airtel',
+                        'frequency': 1800,
+                        'bandwidth': 3
+                    },
+                ],
+                '3_networks': [
+                    {
+                        'name': 'Access Comm',
+                        'frequency': 800,
+                        'bandwidth': 10 #Actually 5, treating as 10 for now
+                    },
+                ],
+            },
         },
     }
