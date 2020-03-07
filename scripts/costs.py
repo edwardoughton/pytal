1152e3bccb7ed028febced59a489523dd096ce37
ej550 <edward.oughton@gmail.com>
2020-03-07 18:18:30 +0000

    Add spectrum cost calculation function

--- scripts/costs.py
+++ scripts/costs.py
@@ -12,30 +12,37 @@ import math
 from utils import discount_capex_and_opex, discount_opex
 
 
-def find_single_network_cost(sites_per_km2, strategy, geotype, costs, parameters):
+def find_single_network_cost(region, sites_per_km2, strategy, geotype, costs,
+    global_parameters, country_parameters):
     """
     Calculates the annual total cost using capex and opex.
 
     Parameters
     ----------
+    region : ??
+        ??
     sites_per_km2 : int
         The density of sites.
     strategy : str
         Infrastructure sharing strategy.
     costs : dict
         Contains the costs of each necessary equipment item.
-    parameters : dict
-        Contains all parameters.
+    global_parameters : dict
+        Contains all global_parameters.
+    country_parameters :
+        ???
 
     Returns
     -------
     output : list of dicts
-        Contains a list of costs, with affliated discounted capex and opex costs.
+        Contains a list of costs, with affliated discounted capex and
+        opex costs.
 
     """
     generation, core, backhaul, sharing = get_strategy_options(strategy)
 
-    cost_breakdown = get_costs(sharing, backhaul, geotype, costs, sites_per_km2, parameters)
+    cost_breakdown = get_costs(region, sharing, backhaul, geotype, costs, sites_per_km2,
+        global_parameters, country_parameters)
 
     total_deployment_costs_km2 = 0
     for key, value in cost_breakdown.items():
@@ -46,37 +53,45 @@ def find_single_network_cost(sites_per_km2, strategy, geotype, costs, parameters
     return cost_breakdown
 
 
-def get_costs(sharing, backhaul, geotype, costs, sites_per_km2, parameters):
+def get_costs(region, sharing, backhaul, geotype, costs, sites_per_km2,
+    global_parameters, country_parameters):
 
     if sharing == 'baseline':
-        costs = baseline(backhaul, geotype, costs, sites_per_km2, parameters)
+        costs = baseline(region, backhaul, geotype, costs, sites_per_km2,
+            global_parameters, country_parameters)
+
     if sharing == 'passive':
-        costs = passive(backhaul, geotype, costs, sites_per_km2, parameters)
+        costs = passive(region, backhaul, geotype, costs, sites_per_km2,
+            global_parameters, country_parameters)
+
     if sharing == 'active':
-        costs = active(backhaul, geotype, costs, sites_per_km2, parameters)
+        costs = active(region, backhaul, geotype, costs, sites_per_km2,
+            global_parameters, country_parameters)
 
     return costs
 
 
-def baseline(backhaul, geotype, costs, sites_per_km2, parameters):
+def baseline(region, backhaul, geotype, costs, sites_per_km2, global_parameters,
+    country_parameters):
     """
     No sharing takes place.
-    Reflects the baseline scenario of needing to build a single dedicated network.
+    Reflects the baseline scenario of needing to build a single dedicated
+    network.
 
     """
     backhaul_cost = get_backhaul_costs(backhaul, geotype, costs)
 
     cost_breakdown = {
         'single_sector_antenna': (
-            discount_capex_and_opex(costs['single_sector_antenna'], parameters) *
-            parameters['sectorization'] * sites_per_km2
+            discount_capex_and_opex(costs['single_sector_antenna'], global_parameters) *
+            global_parameters['sectorization'] * sites_per_km2
         ),
         'single_remote_radio_unit': (
-            discount_capex_and_opex(costs['single_remote_radio_unit'], parameters) *
-            parameters['sectorization'] * sites_per_km2
+            discount_capex_and_opex(costs['single_remote_radio_unit'], global_parameters) *
+            global_parameters['sectorization'] * sites_per_km2
         ),
         'single_baseband_unit': (
-            discount_capex_and_opex(costs['single_baseband_unit'], parameters) *
+            discount_capex_and_opex(costs['single_baseband_unit'], global_parameters) *
             sites_per_km2
         ),
         'tower': (
@@ -92,28 +107,30 @@ def baseline(backhaul, geotype, costs, sites_per_km2, parameters):
             costs['installation'] * sites_per_km2
         ),
         'site_rental': (
-            discount_opex(costs['site_rental'], parameters) * sites_per_km2
+            discount_opex(costs['site_rental'], global_parameters) * sites_per_km2
         ),
         'power_generator_battery_system': (
-            discount_capex_and_opex(costs['power_generator_battery_system'], parameters) *
+            discount_capex_and_opex(costs['power_generator_battery_system'], global_parameters) *
             sites_per_km2
         ),
         'high_speed_backhaul_hub': (
-            discount_capex_and_opex(costs['high_speed_backhaul_hub'], parameters) *
+            discount_capex_and_opex(costs['high_speed_backhaul_hub'], global_parameters) *
             sites_per_km2
         ),
         'router': (
-            discount_capex_and_opex(costs['router'], parameters) * sites_per_km2
+            discount_capex_and_opex(costs['router'], global_parameters) * sites_per_km2
         ),
         '{}_backhaul'.format(backhaul): (
-            discount_capex_and_opex(backhaul_cost, parameters) * sites_per_km2
-        )
+            discount_capex_and_opex(backhaul_cost, global_parameters) * sites_per_km2
+        ),
+        'spectrum': get_spectrum_costs(region, country_parameters)
     }
 
     return cost_breakdown
 
 
-def passive(backhaul, geotype, costs, sites_per_km2, parameters):
+def passive(region, backhaul, geotype, costs, sites_per_km2, global_parameters,
+    country_parameters):
     """
     Sharing of:
         - Mast
@@ -124,55 +141,57 @@ def passive(backhaul, geotype, costs, sites_per_km2, parameters):
 
     cost_breakdown = {
         'single_sector_antenna': (
-            discount_capex_and_opex(costs['single_sector_antenna'], parameters) *
-            parameters['sectorization'] * sites_per_km2
+            discount_capex_and_opex(costs['single_sector_antenna'], global_parameters) *
+            global_parameters['sectorization'] * sites_per_km2
         ),
         'single_remote_radio_unit': (
-            discount_capex_and_opex(costs['single_remote_radio_unit'], parameters) *
-            parameters['sectorization'] * sites_per_km2
+            discount_capex_and_opex(costs['single_remote_radio_unit'], global_parameters) *
+            global_parameters['sectorization'] * sites_per_km2
         ),
         'single_baseband_unit': (
-            discount_capex_and_opex(costs['single_baseband_unit'], parameters) *
+            discount_capex_and_opex(costs['single_baseband_unit'], global_parameters) *
             sites_per_km2
         ),
         'tower': (
-            costs['tower'] * sites_per_km2 / parameters['networks']
+            costs['tower'] * sites_per_km2 / global_parameters['networks']
         ),
         'civil_materials': (
-            costs['civil_materials'] * sites_per_km2 / parameters['networks']
+            costs['civil_materials'] * sites_per_km2 / global_parameters['networks']
         ),
         'transportation': (
-            costs['transportation'] * sites_per_km2 / parameters['networks']
+            costs['transportation'] * sites_per_km2 / global_parameters['networks']
         ),
         'installation': (
-            costs['installation'] * sites_per_km2 / parameters['networks']
+            costs['installation'] * sites_per_km2 / global_parameters['networks']
         ),
         'site_rental': (
-            discount_opex(costs['site_rental'], parameters) *
-            sites_per_km2 / parameters['networks']
+            discount_opex(costs['site_rental'], global_parameters) *
+            sites_per_km2 / global_parameters['networks']
         ),
         'power_generator_battery_system': (
-            discount_capex_and_opex(costs['power_generator_battery_system'], parameters) *
-            sites_per_km2 / parameters['networks']
+            discount_capex_and_opex(costs['power_generator_battery_system'], global_parameters) *
+            sites_per_km2 / global_parameters['networks']
         ),
         'high_speed_backhaul_hub': (
-            discount_capex_and_opex(costs['high_speed_backhaul_hub'], parameters) *
-            sites_per_km2 / parameters['networks']
+            discount_capex_and_opex(costs['high_speed_backhaul_hub'], global_parameters) *
+            sites_per_km2 / global_parameters['networks']
         ),
         'router': (
-            discount_capex_and_opex(costs['router'], parameters) *
-            sites_per_km2 / parameters['networks']
+            discount_capex_and_opex(costs['router'], global_parameters) *
+            sites_per_km2 / global_parameters['networks']
         ),
         '{}_backhaul'.format(backhaul): (
-            discount_capex_and_opex(backhaul_cost, parameters) *
-            sites_per_km2 / parameters['networks']
-        )
+            discount_capex_and_opex(backhaul_cost, global_parameters) *
+            sites_per_km2 / global_parameters['networks']
+        ),
+        'spectrum': get_spectrum_costs(region, country_parameters)
     }
 
     return cost_breakdown
 
 
-def active(backhaul, geotype, costs, sites_per_km2, parameters):
+def active(region, backhaul, geotype, costs, sites_per_km2, global_parameters,
+    country_parameters):
     """
     Sharing of:
         - RAN
@@ -184,55 +203,56 @@ def active(backhaul, geotype, costs, sites_per_km2, parameters):
 
     cost_breakdown = {
         'single_sector_antenna': (
-            discount_capex_and_opex(costs['single_sector_antenna'], parameters) *
-            parameters['sectorization'] * sites_per_km2 /
-            parameters['networks']
+            discount_capex_and_opex(costs['single_sector_antenna'], global_parameters) *
+            global_parameters['sectorization'] * sites_per_km2 /
+            global_parameters['networks']
         ),
         'single_remote_radio_unit': (
-            discount_capex_and_opex(costs['single_remote_radio_unit'], parameters) *
-            parameters['sectorization'] * sites_per_km2 /
-            parameters['networks']
+            discount_capex_and_opex(costs['single_remote_radio_unit'], global_parameters) *
+            global_parameters['sectorization'] * sites_per_km2 /
+            global_parameters['networks']
         ),
         'single_baseband_unit': (
-            discount_capex_and_opex(costs['single_baseband_unit'], parameters) *
-            sites_per_km2 / parameters['networks']
+            discount_capex_and_opex(costs['single_baseband_unit'], global_parameters) *
+            sites_per_km2 / global_parameters['networks']
         ),
         'tower': (
             costs['tower'] *
-            sites_per_km2 / parameters['networks']
+            sites_per_km2 / global_parameters['networks']
         ),
         'civil_materials': (
             costs['civil_materials'] *
-            sites_per_km2 / parameters['networks']
+            sites_per_km2 / global_parameters['networks']
         ),
         'transportation': (
             costs['transportation'] *
-            sites_per_km2 / parameters['networks']
+            sites_per_km2 / global_parameters['networks']
         ),
         'installation': (
             costs['installation'] *
-            sites_per_km2 / parameters['networks']
+            sites_per_km2 / global_parameters['networks']
         ),
         'site_rental': (
-            discount_opex(costs['site_rental'], parameters) *
-            sites_per_km2 / parameters['networks']
+            discount_opex(costs['site_rental'], global_parameters) *
+            sites_per_km2 / global_parameters['networks']
         ),
         'power_generator_battery_system': (
-            discount_capex_and_opex(costs['power_generator_battery_system'], parameters) *
-            sites_per_km2 / parameters['networks']
+            discount_capex_and_opex(costs['power_generator_battery_system'], global_parameters) *
+            sites_per_km2 / global_parameters['networks']
         ),
         'high_speed_backhaul_hub': (
-            discount_capex_and_opex(costs['high_speed_backhaul_hub'], parameters) *
-            sites_per_km2 / parameters['networks']
+            discount_capex_and_opex(costs['high_speed_backhaul_hub'], global_parameters) *
+            sites_per_km2 / global_parameters['networks']
         ),
         'router': (
-            discount_capex_and_opex(costs['router'], parameters) *
-            sites_per_km2 / parameters['networks']
+            discount_capex_and_opex(costs['router'], global_parameters) *
+            sites_per_km2 / global_parameters['networks']
         ),
         '{}_backhaul'.format(backhaul): (
-            discount_capex_and_opex(backhaul_cost, parameters) *
-            sites_per_km2 / parameters['networks']
-        )
+            discount_capex_and_opex(backhaul_cost, global_parameters) *
+            sites_per_km2 / global_parameters['networks']
+        ),
+        'spectrum': get_spectrum_costs(region, country_parameters)
     }
 
     return cost_breakdown
@@ -250,7 +270,10 @@ def get_strategy_options(strategy):
 
 
 def get_backhaul_costs(backhaul, geotype, costs):
+    """
+    Calculate backhaul costs.
 
+    """
     if backhaul == 'microwave':
         tech = '{}_backhaul_{}'.format(backhaul, geotype)
         cost = costs[tech]
@@ -258,5 +281,26 @@ def get_backhaul_costs(backhaul, geotype, costs):
     if backhaul == 'fiber':
         tech = '{}_backhaul_{}'.format(backhaul, geotype)
         cost = costs[tech]
+
+    return cost
+
+
+def get_spectrum_costs(region, country_parameters):
+    """
+    Calculate spectrum costs.
+
+    """
+    population = int(round(region['population']))
+    frequencies = country_parameters['frequencies']
+    networks = country_parameters['networks']
+    frequencies = frequencies['{}_networks'.format(networks)]
+
+    for frequency in frequencies:
+        if frequency['frequency'] < 1000:
+            cost_usd_mhz_pop = country_parameters['costs']['spectrum_coverage_usd_mhz_pop']
+        else:
+            cost_usd_mhz_pop = country_parameters['costs']['spectrum_capacity_usd_mhz_pop']
+
+    cost = cost_usd_mhz_pop * frequency['bandwidth'] * population
 
     return cost
