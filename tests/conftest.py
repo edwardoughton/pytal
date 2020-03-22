0edb96502a25bf2f2b18bc7293ef6f7e7d0ec381
ej550 <edward.oughton@gmail.com>
2020-03-22 13:57:02 +0000

    Add conf fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,7 @@ from pytest import fixture
 def setup_region():
     return [{
     'GID_0': 'MWI',
-    'GID_3': 'MWI.1.1.1_1',
-    'median_luminosity': 20.5,
-    'sum_luminosity': 41.0,
+    'GID_id': 'MWI.1.1.1_1',
     'mean_luminosity_km2': 26.736407691655717,
     'population': 10000,
     'area_km2': 2,
@@ -28,8 +26,8 @@ def setup_option():
 @fixture(scope='function')
 def setup_global_parameters():
     return {
-        'overbooking_factor': 50,
-        'return_period': 10,
+        'overbooking_factor': 100,
+        'return_period': 2,
         'discount_rate': 5,
         'opex_percentage_of_capex': 10,
         'sectorization': 3,
@@ -57,82 +55,30 @@ def setup_country_parameters():
         'networks': 3,
         # https://en.wikipedia.org/wiki/List_of_LTE_networks_in_Africa
         'frequencies': {
-            '4G': {
-                    '1_networks': [
-                        {
-                            'name': 'Access Comm',
-                            'frequency': 800, #Actually 850 MHz but treating as 800 MHz
-                            'bandwidth': 10 #Actually 5, treating as 10 for now
-                        },
-                        {
-                            'name': 'Airtel',
-                            'frequency': 1800,
-                            'bandwidth': 3
-                        },
-                        {
-                            'name': 'TNM',
-                            'frequency': 2500,
-                            'bandwidth': 41
-                        },
-                    ],
-                '2_networks': [
-                    {
-                        'name': 'Access Comm',
-                        'frequency': 800,
-                        'bandwidth': 10 #Actually 5, treating as 10 for now
-                    },
-                    {
-                        'name': 'Airtel',
-                        'frequency': 1800,
-                        'bandwidth': 3
-                    },
-                ],
-                '3_networks': [
-                    {
-                        'name': 'Access Comm',
-                        'frequency': 800,
-                        'bandwidth': 10 #Actually 5, treating as 10 for now
-                    },
-                ],
-            },
-            '5G': {
-                '1_networks': [
-                    {
-                        'name': 'Access Comm',
-                        'frequency': 700,
-                        'bandwidth': 10,
-                    },
-                    {
-                        'name': 'Airtel',
-                        'frequency': 700,
-                        'bandwidth': 10,
-                    },
-                    {
-                        'name': 'TNM',
-                        'frequency': 700,
-                        'bandwidth': 10,
-                    },
-                ],
-                '2_networks': [
-                    {
-                        'name': 'Access Comm',
-                        'frequency': 700,
-                        'bandwidth': 10,
-                    },
-                    {
-                        'name': 'Airtel',
-                        'frequency': 700,
-                        'bandwidth': 10,
-                    },
-                ],
-                '3_networks': [
-                    {
-                        'name': 'Access Comm',
-                        'frequency': 700,
-                        'bandwidth': 10,
-                    },
-                ],
-            }
+            '4G': [
+                {
+                    'frequency': 800,
+                    'bandwidth': 10,
+                },
+                {
+                    'frequency': 1800,
+                    'bandwidth': 10,
+                },
+            ],
+            '5G': [
+                {
+                    'frequency': 700,
+                    'bandwidth': 10,
+                },
+                {
+                    'frequency': 3500,
+                    'bandwidth': 50,
+                },
+                {
+                    'frequency': 26000,
+                    'bandwidth': 500,
+                },
+            ]
         },
         'costs': {
             'spectrum_coverage_usd_mhz_pop': 0.5,
@@ -145,46 +91,105 @@ def setup_country_parameters():
 def setup_timesteps():
     return [
         2020,
-        2021,
-        2022,
-        2023,
-        2024,
-        2025,
-        2026,
-        2027,
-        2028,
-        2029,
-        2030
+        # 2021,
+        # 2022,
+        # 2023,
+        # 2024,
+        # 2025,
+        # 2026,
+        # 2027,
+        # 2028,
+        # 2029,
+        # 2030
     ]
 
 
 @fixture(scope='function')
 def setup_penetration_lut():
     return {
-        2005: 3.34,
-        2006: 4.78,
-        2007: 7.88,
-        2008: 10.98,
-        2009: 17.59,
-        2010: 21.44,
-        2011: 26.41,
-        2012: 30.18,
-        2013: 33.4,
-        2014: 34.58,
-        2015: 39.22,
-        2016: 41.72,
-        2017: 43.99,
-        2018: 45.15,
-        2019: 46.35,
-        2020: 47.58,
-        2021: 48.84,
-        2022: 50.13,
-        2023: 51.46,
-        2024: 52.82,
-        2025: 54.22,
-        2026: 55.65,
-        2027: 57.13,
-        2028: 58.64,
-        2029: 60.19,
-        2030: 61.78
+        2020: 50,
+        # 2021: 75,
+    }
+
+
+@fixture(scope='function')
+def setup_costs():
+    return {
+        #all costs in $USD
+        'single_sector_antenna': 1500,
+        'single_remote_radio_unit': 4000,
+        'single_baseband_unit': 10000,
+        'tower': 10000,
+        'civil_materials': 5000,
+        'transportation': 10000,
+        'installation': 5000,
+        'site_rental': 9600,
+        'power_generator_battery_system': 5000,
+        'high_speed_backhaul_hub': 15000,
+        'router': 2000,
+        'microwave_backhaul_small': 10000,
+        'microwave_backhaul_medium': 20000,
+        'microwave_backhaul_large': 40000,
+        'fiber_backhaul_urban_m': 5,
+        'fiber_backhaul_suburban_m': 5,
+        'fiber_backhaul_rural_m': 5,
+        'core_nodes_epc': 100000,
+        'core_nodes_nsa': 150000,
+        'core_nodes_sa': 200000,
+        'core_edges': 20,
+        'regional_nodes_epc': 100000,
+        'regional_nodes_nsa': 150000,
+        'regional_nodes_sa': 200000,
+        'regional_edges': 10,
+    }
+
+
+@fixture(scope='function')
+def setup_lookup():
+    return {
+        ('urban', 'macro', '800', '4G', '50'): [
+            (0.01, 1),
+            (0.02, 2),
+            (0.05, 5),
+            (0.15, 15),
+            (2, 100)
+        ],
+        ('urban', 'macro', '1800', '4G', '50'): [
+            (0.01, 5),
+            (0.02, 10),
+            (0.05, 20),
+            (0.15, 40),
+            (2, 1000)
+        ],
+
+    }
+
+
+@fixture(scope='function')
+def setup_ci():
+    return 50
+
+
+@fixture(scope='function')
+def setup_backhaul_lut():
+    return {
+        'MWI.1.1.1_1': 1000
+    }
+
+
+@fixture(scope='function')
+def setup_core_lut():
+    return {
+        'core_edges': {
+            'MWI.1.1.1_1': 1000
+        },
+        'core_nodes': {
+            'MWI.1.1.1_1': 2
+        },
+        'regional_edges': {
+            'MWI.1.1.1_1': 1000
+        },
+        'regional_nodes': {
+            'MWI.1.1.1_1': 2
+        },
     }
