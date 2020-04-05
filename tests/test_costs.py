cc59a5f7e843e6c715cae615507580a474228586
ej550 <edward.oughton@gmail.com>
2020-04-05 11:57:39 +0100

    interim push before rewrite

--- tests/test_costs.py
+++ tests/test_costs.py
@@ -1,17 +1,85 @@
 import pytest
-from pytal.costs import greenfield_4g, upgrade_to_4g
+from pytal.costs import (greenfield_4g, upgrade_to_4g, greenfield_5g_nsa,
+    upgrade_to_5g_nsa, greenfield_5g_sa, upgrade_to_5g_sa,
+    get_backhaul_costs, get_core_costs, discount_opex, discount_capex_and_opex)
 
-    # baseline_new_site, baseline_upgrade, passive_new_site, active_new_site,
-    # get_backhaul_costs, get_core_costs, discount_capex_and_opex, discount_opex)
+
+# def test_greenfield_4g(setup_region, setup_option, setup_costs,
+#     setup_global_parameters, setup_backhaul_lut, setup_core_lut):
+
+#     setup_region[0]['new_sites'] = 1
+
+#     total_cost, cost_structure = greenfield_4g('MWI', setup_region[0],
+#         '4G_epc_microwave_baseline_baseline_baseline_baseline',
+#         setup_costs, setup_global_parameters,
+#         setup_backhaul_lut, setup_core_lut)
+
+#     assert cost_structure['antennas'] == 5379
+#     assert cost_structure['remote_radio_units'] == 14343
+#     assert cost_structure['single_baseband_unit'] == 11952
+#     assert cost_structure['tower'] == 10000
+#     assert cost_structure['civil_materials'] == 10000
+#     assert cost_structure['transportation'] == 10000
+#     assert cost_structure['installation'] == 5000
+#     assert cost_structure['site_rental'] == 18743
+#     assert cost_structure['power_generator_battery_system'] == 5976
+#     assert cost_structure['high_speed_backhaul_hub'] == 17929
+#     assert cost_structure['router'] == 2390
+#     assert cost_structure['microwave_backhaul'] == 11952
+
+#     assert total_cost == 637617
+
+#     total_cost, cost_structure = greenfield_4g('MWI', setup_region[0],
+#         '4G_epc_microwave_passive_baseline_baseline_baseline',
+#         setup_costs, setup_global_parameters,
+#         setup_backhaul_lut, setup_core_lut)
+
+#     assert cost_structure['tower'] == 5000
+#     assert cost_structure['civil_materials'] == 5000
+
+#     assert total_cost == 607757.5
+
+#     # total_cost, cost_structure = greenfield_4g('MWI', setup_region[0],
+#     #     '4G_epc_microwave_active_baseline_baseline_baseline',
+#     #     setup_costs, setup_global_parameters,
+#     #     setup_backhaul_lut, setup_core_lut)
+
+#     # assert cost_structure['antennas'] == (1500*3) / setup_global_parameters['networks']
+#     # assert cost_structure['civil_materials'] == 5000
+
+#     # assert total_cost == 607757.5
 
 
-def test_greenfield_4g(setup_region, setup_option, setup_costs,
+# def test_upgrade_to_4g(setup_region, setup_option, setup_costs,
+#     setup_global_parameters, setup_backhaul_lut, setup_core_lut):
+
+#     setup_region[0]['upgraded_sites'] = 1
+#     setup_region[0]['sites_3G'] = 1
+
+#     total_cost, cost_structure = upgrade_to_4g('MWI', setup_region[0],
+#         '4G_epc_microwave_baseline_baseline_baseline_baseline',
+#         setup_costs, setup_global_parameters,
+#         setup_backhaul_lut, setup_core_lut)
+
+#     assert cost_structure['antennas'] == 5379
+#     assert cost_structure['remote_radio_units'] == 14343
+#     assert cost_structure['single_baseband_unit'] == 11952
+#     assert cost_structure['installation'] == 5000
+#     assert cost_structure['high_speed_backhaul_hub'] == 17929
+#     assert cost_structure['router'] == 2390
+#     assert cost_structure['microwave_backhaul'] == 11952
+
+#     assert total_cost == 582898
+
+
+def test_greenfield_5g_nsa(setup_region, setup_option, setup_costs,
     setup_global_parameters, setup_backhaul_lut, setup_core_lut):
 
     setup_region[0]['new_sites'] = 1
 
-    total_cost, cost_structure = greenfield_4g('MWI', setup_region[0],
-        setup_option['strategy'], setup_costs, setup_global_parameters,
+    total_cost, cost_structure = greenfield_5g_nsa('MWI', setup_region[0],
+        '5G_nsa_microwave_baseline_baseline_baseline_baseline',
+        setup_costs, setup_global_parameters,
         setup_backhaul_lut, setup_core_lut)
 
     assert cost_structure['antennas'] == 5379
@@ -30,35 +98,37 @@ def test_greenfield_4g(setup_region, setup_option, setup_costs,
     assert total_cost == 637617
 
 
-def test_upgrade_to_4g(setup_region, setup_option, setup_costs,
-    setup_global_parameters, setup_backhaul_lut, setup_core_lut):
+# def test_upgrade_to_5g_nsa(setup_region, setup_option, setup_costs,
+#     setup_global_parameters, setup_backhaul_lut, setup_core_lut):
 
-    setup_region[0]['upgraded_sites'] = 1
-    setup_region[0]['sites_3G'] = 1
+#     setup_region[0]['upgraded_sites'] = 1
+#     setup_region[0]['sites_3G'] = 1
 
-    total_cost, cost_structure = upgrade_to_4g('MWI', setup_region[0],
-        setup_option['strategy'], setup_costs, setup_global_parameters,
-        setup_backhaul_lut, setup_core_lut)
+#     total_cost, cost_structure = upgrade_to_5g_nsa('MWI', setup_region[0],
+#         '5G_nsa_microwave_baseline_baseline_baseline_baseline',
+#         setup_costs, setup_global_parameters,
+#         setup_backhaul_lut, setup_core_lut)
 
-    assert cost_structure['antennas'] == 5379
-    assert cost_structure['remote_radio_units'] == 14343
-    assert cost_structure['single_baseband_unit'] == 11952
-    assert cost_structure['installation'] == 5000
-    assert cost_structure['high_speed_backhaul_hub'] == 17929
-    assert cost_structure['router'] == 2390
-    assert cost_structure['microwave_backhaul'] == 11952
+#     assert cost_structure['antennas'] == 5379
+#     assert cost_structure['remote_radio_units'] == 14343
+#     assert cost_structure['single_baseband_unit'] == 11952
+#     assert cost_structure['installation'] == 5000
+#     assert cost_structure['high_speed_backhaul_hub'] == 17929
+#     assert cost_structure['router'] == 2390
+#     assert cost_structure['microwave_backhaul'] == 11952
 
-    assert total_cost == 582898
+#     assert total_cost == 582898
 
 
-
-# def test_baseline_new_site(setup_region, setup_option, setup_costs, setup_global_parameters,
-#     setup_backhaul_lut, setup_core_lut):
+# def test_greenfield_5g_sa(setup_region, setup_option, setup_costs,
+#     setup_global_parameters, setup_backhaul_lut, setup_core_lut):
 
 #     setup_region[0]['new_sites'] = 1
 
-#     total_cost, cost_structure = baseline_new_site('MWI', setup_region[0], setup_option['strategy'],
-#         setup_costs, setup_global_parameters, setup_backhaul_lut, setup_core_lut)
+#     total_cost, cost_structure = greenfield_5g_sa('MWI', setup_region[0],
+#         '5G_sa_microwave_baseline_baseline_baseline_baseline',
+#         setup_costs, setup_global_parameters,
+#         setup_backhaul_lut, setup_core_lut)
 
 #     assert cost_structure['antennas'] == 5379
 #     assert cost_structure['remote_radio_units'] == 14343
@@ -75,90 +145,27 @@ def test_upgrade_to_4g(setup_region, setup_option, setup_costs,
 
 #     assert total_cost == 637617
 
-#     setup_region[0]['new_sites'] = 10
 
-#     total_cost, cost_structure = baseline_new_site('MWI', setup_region[0], setup_option['strategy'],
-#         setup_costs, setup_global_parameters, setup_backhaul_lut, setup_core_lut)
+# def test_upgrade_to_5g_sa(setup_region, setup_option, setup_costs,
+#     setup_global_parameters, setup_backhaul_lut, setup_core_lut):
 
-#     assert total_cost == 6376170
+#     setup_region[0]['upgraded_sites'] = 1
+#     setup_region[0]['sites_3G'] = 1
 
-
-# def test_baseline_upgrade(setup_costs, setup_global_parameters):
-
-#     total_cost, cost_structure = baseline_upgrade(1, setup_costs, setup_global_parameters)
+#     total_cost, cost_structure = upgrade_to_5g_sa('MWI', setup_region[0],
+#         '5G_sa_microwave_baseline_baseline_baseline_baseline',
+#         setup_costs, setup_global_parameters,
+#         setup_backhaul_lut, setup_core_lut)
 
 #     assert cost_structure['antennas'] == 5379
 #     assert cost_structure['remote_radio_units'] == 14343
 #     assert cost_structure['single_baseband_unit'] == 11952
 #     assert cost_structure['installation'] == 5000
+#     assert cost_structure['high_speed_backhaul_hub'] == 17929
+#     assert cost_structure['router'] == 2390
+#     assert cost_structure['microwave_backhaul'] == 11952
 
-#     assert total_cost == 36674
-
-#     total_cost, cost_structure = baseline_upgrade(10, setup_costs, setup_global_parameters)
-
-#     assert total_cost == 366740
-
-
-# def test_passive_new_site(setup_region, setup_option, setup_costs, setup_global_parameters,
-#     setup_backhaul_lut, setup_core_lut):
-
-#     setup_region[0]['new_sites'] = 1
-
-#     total_cost, cost_structure = passive_new_site('MWI', setup_region[0], setup_option['strategy'],
-#         setup_costs, setup_global_parameters, setup_backhaul_lut, setup_core_lut)
-
-#     assert cost_structure['antennas'] == 5379
-#     assert cost_structure['remote_radio_units'] == 14343
-#     assert cost_structure['single_baseband_unit'] == 11952
-#     assert cost_structure['tower'] == 5000
-#     assert cost_structure['civil_materials'] == 5000
-#     assert cost_structure['transportation'] == 5000
-#     assert cost_structure['installation'] == 2500
-#     assert cost_structure['site_rental'] == 9371.5
-#     assert cost_structure['power_generator_battery_system'] == 2988
-#     assert cost_structure['high_speed_backhaul_hub'] == 8964.5
-#     assert cost_structure['router'] == 1195
-#     assert cost_structure['microwave_backhaul'] == 5976
-
-#     assert total_cost == 591622.0
-
-#     setup_region[0]['new_sites'] = 10
-
-#     total_cost, cost_structure = passive_new_site('MWI', setup_region[0], setup_option['strategy'],
-#     setup_costs, setup_global_parameters, setup_backhaul_lut, setup_core_lut)
-
-#     assert total_cost == 5916220
-
-
-# def test_active_new_site(setup_region, setup_option, setup_costs, setup_global_parameters,
-#     setup_backhaul_lut, setup_core_lut):
-
-#     setup_region[0]['new_sites'] = 1
-
-#     total_cost, cost_structure = active_new_site('MWI', setup_region[0], setup_option['strategy'],
-#     setup_costs, setup_global_parameters, setup_backhaul_lut, setup_core_lut)
-
-#     assert cost_structure['antennas'] == 2689.5
-#     assert cost_structure['remote_radio_units'] == 7171.5
-#     assert cost_structure['single_baseband_unit'] == 5976
-#     assert cost_structure['tower'] == 5000
-#     assert cost_structure['civil_materials'] == 5000
-#     assert cost_structure['transportation'] == 5000
-#     assert cost_structure['installation'] == 2500
-#     assert cost_structure['site_rental'] == 9371.5
-#     assert cost_structure['power_generator_battery_system'] == 2988
-#     assert cost_structure['high_speed_backhaul_hub'] == 8964.5
-#     assert cost_structure['router'] == 1195
-#     assert cost_structure['microwave_backhaul'] == 5976
-
-#     assert total_cost == 318808.5
-
-#     setup_region[0]['new_sites'] = 10
-
-#     total_cost, cost_structure = active_new_site('MWI', setup_region[0], setup_option['strategy'],
-#         setup_costs, setup_global_parameters, setup_backhaul_lut, setup_core_lut)
-
-#     assert total_cost == 3188085
+#     assert total_cost == 582898
 
 
 # def test_get_backhaul_costs(setup_region, setup_costs, setup_backhaul_lut):
