476d323418648e6c89832b6c91c250a30a738c53
ej550 <edward.oughton@gmail.com>
2020-04-20 19:51:27 +0100

    Update spectrum prices

--- vis/spectrum/spectrum.r
+++ vis/spectrum/spectrum.r
@@ -1,6 +1,7 @@
 #Spectrum costs
 library(tidyverse)
 require("ggrepel")
+library(ggpubr)
 
 #get folder directory
 folder <- dirname(rstudioapi::getSourceEditorContext()$path)
@@ -61,17 +62,48 @@ capacity <- ggplot(capacity, aes(x=year, y=dollars.mhz.pop, colour=income, shape
        subtitle = "Prices adjusted for PPP exchange rates, inflation and license duration, and include annual fees."
       )
 
-library(ggpubr)
 
 panel <- ggarrange(coverage, capacity, ncol = 1, nrow = 2, align = c("hv"), common.legend = TRUE, legend="bottom")
-
-# #export to folder
-# path = file.path(folder, 'figures', 'panel.tiff')
-# tiff(path, units="in", width=10, height=10, res=300)
-# print(panel)
-# dev.off()
 
 path = file.path(folder, 'figures', 'panel.png')
 ggsave(path, units="in", width=8, height=12)
 print(panel)
 dev.off()
+
+#get folder directory
+folder <- dirname(rstudioapi::getSourceEditorContext()$path)
+
+data <- read.csv(file.path(folder, 'data_inputs', 'spectrum_by_cluster_country.csv'))
+
+data$income[data$Country == 'Uganda'] <- 'Lower'
+data$income[data$Country == 'Kenya'] <- 'Lower Middle'
+data$income[data$Country == 'Senegal'] <- 'Lower Middle'
+data$income[data$Country == 'Pakistan'] <- 'Lower Middle'
+data$income[data$Country == 'Albania'] <- 'Upper Middle'
+data$income[data$Country == 'Peru'] <- 'Upper Middle'
+data$income[data$Country == 'Mexico'] <- 'Upper Middle'
+
+data$income = factor(data$income, levels=c("Upper Middle", "Lower Middle", "Lower"))
+
+cluster_prices <- ggplot(data, aes(x=Year, y=usd_per_mhz_per_pop, colour=income))  + 
+  geom_point(size=3) +
+  geom_label_repel(aes(label = Country), size = 3) +
+  scale_x_continuous(limits = c(2010,2018),
+                     breaks= c(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)) +
+  scale_y_continuous(limits=c(0, 0.2)) +
+  theme(axis.text.x = element_text(angle = 30)) +
+  guides(colour=guide_legend(title="Income")) +
+  labs(title = "Spectrum Prices", x=NULL, y='$ / MHz / Per Capita (USD)')
+
+#export to folder
+path = file.path(folder, 'figures', 'cluster_prices.png')
+ggsave(path, units="in", width=8, height=8)
+print(cluster_prices)
+dev.off()
+
+subset <- select(data, Country, usd_per_mhz_per_pop)
+
+mean_spectrum_prices <- aggregate(x = subset$usd_per_mhz_per_pop,                
+                                  by = list(subset$Country),              
+                                  FUN = mean) 
+               
