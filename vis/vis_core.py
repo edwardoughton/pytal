6159f1cd99c30af023ab5313a06551ab5968fb48
ej550 <edward.oughton@gmail.com>
2020-05-06 12:39:51 +0100

    Update vis code for core network

--- vis/vis_core.py
+++ vis/vis_core.py
@@ -19,17 +19,17 @@ DATA_RAW = os.path.join(BASE_PATH, 'raw')
 DATA_INTERMEDIATE = os.path.join(BASE_PATH, 'intermediate')
 DATA_PROCESSED = os.path.join(BASE_PATH, 'processed')
 
-fig, axs = plt.subplots(4, 2, figsize=(20,20))
+fig, axs = plt.subplots(4, 2, figsize=(20, 20))
 
 countries = [
-    ('UGA', 0, 0, 'A'),
-    ('MWI', 0, 1, 'B'),
-    ('KEN', 1, 0, 'C'),
-    ('SEN', 1, 1, 'D'),
-    ('PAK', 2, 0, 'E'),
-    ('ALB', 2, 1, 'F'),
-    ('PER', 3, 0, 'G'),
-    ('MEX', 3, 1, 'H'),
+    ('UGA', 0, 0, 'Uganda (C1)', 3),
+    ('MWI', 0, 1, 'Malawi (C1)', 3.8),
+    ('KEN', 1, 0, 'Kenya (C2)', 5),
+    ('SEN', 1, 1, 'Senegal (C2)', 3),
+    ('PAK', 2, 0, 'Pakistan (C3)', 7.5),
+    ('ALB', 2, 1, 'Albania (C4)', 2),
+    ('PER', 3, 0, 'Peru (C5)', 9),
+    ('MEX', 3, 1, 'Mexico (C6)', 10),
 ]
 
 for country in countries:
@@ -38,32 +38,33 @@ for country in countries:
     x = country[1]
     y = country[2]
     plot_title = country[3]
+    buff_value = country[4]
 
     country_path = os.path.join(DATA_INTERMEDIATE, iso3)
 
     region = gpd.read_file(os.path.join(country_path, 'national_outline.shp'))
-    region.plot(facecolor="none", edgecolor='black', lw=0.7, ax=axs[x, y])
+    region.plot(facecolor="none", edgecolor='grey', lw=1.2, ax=axs[x, y])
 
     core_edges = gpd.read_file(os.path.join(country_path, 'network', 'core_edges.shp'))
     core_nodes = gpd.read_file(os.path.join(country_path, 'network', 'core_nodes.shp'))
 
     existing_edges = core_edges.loc[core_edges['source'] == 'existing']
-    existing_edges.plot(facecolor="none", edgecolor='red', lw=1, ax=axs[x, y])
+    existing_edges.plot(facecolor="none", edgecolor='black', lw=1.5, ax=axs[x, y])
     existing_nodes = core_nodes.loc[core_nodes['source'] == 'existing']
-    existing_nodes.plot(edgecolor='red', markersize=40, marker='.', ax=axs[x, y])
+    existing_nodes.plot(edgecolor='black', markersize=40, marker='.', ax=axs[x, y])
 
     new_edges = core_edges.loc[core_edges['source'] == 'new']
-    new_edges.plot(facecolor="none", edgecolor='orange', lw=1, ax=axs[x, y])
+    new_edges.plot(facecolor="none", edgecolor='red', lw=1.5, ax=axs[x, y])
     new_nodes = core_nodes.loc[core_nodes['source'] == 'new']
-    new_nodes.plot(edgecolor='orange', markersize=40, marker='.', ax=axs[x, y])
+    new_nodes.plot(edgecolor='red', markersize=40, marker='.', ax=axs[x, y])
 
     regional_edges = gpd.read_file(os.path.join(country_path, 'network', 'regional_edges.shp'))
-    regional_edges.plot(facecolor="none", edgecolor='green', lw=0.5, ax=axs[x, y])
+    regional_edges.plot(facecolor="none", edgecolor='orange', lw=0.8, ax=axs[x, y])
     regional_nodes = gpd.read_file(os.path.join(country_path, 'network', 'regional_nodes.shp'))
-    regional_nodes.plot(edgecolor='green', markersize=40, marker='.', ax=axs[x, y])
+    regional_nodes.plot(edgecolor='yellow', markersize=40, marker='.', ax=axs[x, y])
 
     axs[x, y].set_title(plot_title)
-    centroid = region['geometry'].values[0].representative_point().buffer(10).envelope
+    centroid = region['geometry'].values[0].representative_point().buffer(buff_value).envelope
 
     xmin = min([coord[0] for coord in centroid.exterior.coords])
     xmax = max([coord[0] for coord in centroid.exterior.coords])
@@ -73,8 +74,9 @@ for country in countries:
 
     axs[x, y].set_xlim([xmin, xmax])
     axs[x, y].set_ylim([ymin, ymax])
+
     ctx.add_basemap(axs[x, y], crs=region.crs)
 
 fig.tight_layout()
 
-plt.savefig(os.path.join(BASE_PATH, '..', 'vis', 'figures', 'test.png'))
+plt.savefig(os.path.join(BASE_PATH, '..', 'vis', 'figures', 'core_network_plot.png'))
